e6a0d90e1855350c2324e3857f77b5e302716888
maher <matt@gommmd.com>
2016-06-30 18:39:16 -0400

    Holder and Adapter

---
+++ app/src/main/java/com/mmmd/maher/radioapp/adapters/StationsAdapter.java
@@ -0,0 +1,27 @@
+package com.mmmd.maher.radioapp.adapters;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+import com.mmmd.maher.radioapp.holders.StationViewHolder;
+
+/**
+ * Created by maher on 6/30/16.
+ */
+public class StationsAdapter extends RecyclerView.Adapter<StationViewHolder> {
+
+    @Override
+    public void onBindViewHolder(StationViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    @Override
+    public StationViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+}
---
+++ app/src/main/java/com/mmmd/maher/radioapp/holders/StationViewHolder.java
@@ -0,0 +1,14 @@
+package com.mmmd.maher.radioapp.holders;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+/**
+ * Created by maher on 6/30/16.
+ */
+public class StationViewHolder extends RecyclerView.ViewHolder {
+
+    public StationViewHolder(View itemView) {
+        super(itemView);
+    }
+}
